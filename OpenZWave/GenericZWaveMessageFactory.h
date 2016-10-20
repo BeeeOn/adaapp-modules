93b6f4cf9e7f152c127392fa6450caa2d46c2efd
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2016-10-20 20:33:39 +0200

    openzwave: new class GenericZWaveMessageFactory
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
    Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ OpenZWave/GenericZWaveMessageFactory.h
@@ -0,0 +1,28 @@
+/**
+ * @file GenericZWaveMessageFactory.h
+ * @Author BeeeOn team - Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
+ * @date September, 2016
+ */
+
+#pragma once
+
+#include <map>
+
+#include <Poco/Exception.h>
+
+#include "ZWaveMessageFactory.h"
+
+class GenericZWaveMessageFactory : public ZWaveMessageFactory {
+public:
+	ZWaveMessage *create(const uint32_t &manufacturer, const uint32_t &product) override;
+
+	/*
+	 * Register Z-Wave manufacturer to factory using manufacturer id.
+	 * @param &manufacturer Unique number of manufacturer
+	 * @param &factory Manufacturer object
+	 */
+	void registerManufacturer(const uint32_t &manufacturer, ZWaveMessageFactory *factory);
+
+private:
+	std::map<const uint32_t, ZWaveMessageFactory *> m_manufacturers;
+};
